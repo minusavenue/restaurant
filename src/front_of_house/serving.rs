3f26fc404d0e989f131d0e0e51ef6c69478b0e82
Christopher Roberts <djabsynth@gmail.com>
2023-01-05 09:51:37 -0800

    module learning

---
+++ src/front_of_house/serving.rs
@@ -0,0 +1,11 @@
+fn take_order() -> ! {
+    todo!()
+}
+
+pub fn serve_order() -> ! {
+    todo!()
+}
+
+fn take_payment() -> ! {
+    todo!()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,61 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() -> ! {
-            todo!()
+use rand::{CryptoRng, ErrorKind::Transient, Rng};
+use std::io::{self, Write};
+
+mod front_of_house;
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
         }
 
-        fn seat_at_table() -> ! {
-            todo!()
+        pub fn new() -> Breakfast {
+            Breakfast {
+                toast: String::from("Chef's Choice"),
+                seasonal_fruit: String::from("Chef's Choice"),
+            }
         }
     }
 
-    mod serving {
-        fn take_order() -> ! {
-            todo!()
-        }
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 
-        fn serve_order() -> ! {
-            todo!()
-        }
+    fn cook_order() {}
 
-        fn take_payment() -> ! {
-            todo!()
-        }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
     }
 }
 
+use self::back_of_house::Appetizer;
+use self::back_of_house::Breakfast;
+use self::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     //Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
-    //Relative path
-    front_of_house::hosting::add_to_waitlist();
+    let mut meal = Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    let quick_order = Breakfast::new();
+
+    let order1 = Appetizer::Soup;
+    let order2 = Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
